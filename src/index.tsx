de6aaa1cccb759a4a69a1fd5557af5d379980d92
Ismail Güven <ismail.guven@resurs.se>
2024-12-02 09:22:26 +0100

    fix:1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import "whatwg-fetch";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found.");
+}
