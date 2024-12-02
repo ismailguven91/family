62dd5a9c883573bdc9332d32a42cde02a4f56665
Ismail Güven <ismail.guven@resurs.se>
2024-12-02 10:28:20 +0100

    fix:1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import "core-js";
+import "regenerator-runtime/runtime";
 
 const rootElement = document.getElementById("root");
 
